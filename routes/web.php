3672a83844c875d3d2c5b9461fcdc17c1ebd1256
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-23 18:54:54 +0800

    2.2.1

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\PaymentController;
 Route::middleware(['web', 'auth'])->group(function () {
-    Route::get('/admin-plugin', function () {
-        if (Auth::check()) {
-            $user = Auth::user();
-            // Do something with $user if needed
-        }
-
-        return view('adminsystem::admin');
-    })->name('admin.plugin');
-    Route::get('/tuition-payments', [\App\Plugins\TuitionPayment\Controllers\TuitionPaymentController::class, 'index']);
+    Route::get('/tuition-payments', [\App\Plugins\TuitionPayment\Controllers\TuitionPaymentController::class, 'index'])->name('admin.plugin');
     Route::post('/tuition-payments', [\App\Plugins\TuitionPayment\Controllers\TuitionPaymentController::class, 'store']);
 
 });
