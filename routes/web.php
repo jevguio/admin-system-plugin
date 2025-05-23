5f03ea9454f6fe81af8c342deaa8e64f1368de1d
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-23 19:04:06 +0800

    migration added

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,20 @@ Route::middleware(['web', 'auth'])->group(function () {
 
 });
 
+$pluginMigrationPath = __DIR__ . '/migrations';
+
+if (File::exists($pluginMigrationPath)) {
+    $migrator = app('migrator');
+    $migrator->path($pluginMigrationPath);
+    
+    $migrationFiles = $migrator->getMigrationFiles($pluginMigrationPath);
+    $ranMigrations = $migrator->getRepository()->getRan();
+
+    foreach ($migrationFiles as $migration => $path) {
+        if (!in_array($migration, $ranMigrations)) {
+            include_once $path;
+            (new (require $path))->up(); // manually call up()
+            $migrator->getRepository()->log($migration, 1);
+        }
+    }
+}
